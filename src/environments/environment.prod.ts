08b6789c1a9e9cae96f66d01701d95a2b730a110
kumar-pun <kpun@savadev.com>
2019-06-20 17:23:56 +0545

    chat db

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   userUrl: 'https://kumar-api02.herokuapp.com/api/v1',
-  chatUrl: 'http://localhost:3000/chat'
+  chatUrl: 'https://hrms-chat1.herokuapp.com/chat'
 
 };
