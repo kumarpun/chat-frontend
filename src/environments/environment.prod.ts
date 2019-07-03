7e804c9a63a857227cd2f1195f0b7d5fc65ea19d
kumar-pun <kpun@savadev.com>
2019-07-03 08:07:10 +0545

    production fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,10 @@
+const BASE_URL = 'http://localhost:8000';
+
 export const environment = {
   production: true,
   userUrl: 'https://kumar-api02.herokuapp.com/api/v1',
-  chatUrl: 'https://hrms-chat1.herokuapp.com/chat'
-
+  chatUrls: 'https://hrms-chat1.herokuapp.com/chat',
+  chatUrl: BASE_URL,
+  backendUrl: `${BASE_URL}/api`,
+  chatPath: '/mean-chat-app.io',
 };
